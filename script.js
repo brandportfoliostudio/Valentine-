cf4689b88a4f62ccb7ff23ba580ca06e4d1d41fd
brandportfolio.studio <ameyg1806@gmail.com>
2026-02-13 01:33:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
+const music = document.getElementById("bgMusic");
+const flip = document.getElementById("flipSound");
 
 function openBook(){
 document.getElementById("intro").style.display="none";
 document.getElementById("book").style.display="block";
 document.querySelector(".controls").style.display="block";
+music.play();
 showPage();
 }
 
@@ -26,6 +29,7 @@ page.style.zIndex=1;
 function nextPage(){
 if(currentPage < pages.length-1){
 currentPage++;
+flip.play();
 showPage();
 }
 }
@@ -33,6 +37,14 @@ showPage();
 function prevPage(){
 if(currentPage > 0){
 currentPage--;
+flip.play();
 showPage();
 }
 }
+
+/* Cursor glow */
+const cursor=document.getElementById("cursorGlow");
+document.addEventListener("mousemove",(e)=>{
+cursor.style.left=e.clientX+"px";
+cursor.style.top=e.clientY+"px";
+});
