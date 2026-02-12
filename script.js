c845af54861e60e3ff0f2803e634172551374464
brandportfolio.studio <ameyg1806@gmail.com>
2026-02-13 01:10:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = function(){
+setTimeout(()=>{
+document.getElementById("loader").style.display="none";
+},2000);
+};
+
 const popup=document.getElementById('popup');
 const yesBtn=document.getElementById('yesBtn');
 const noBtn=document.getElementById('noBtn');
@@ -15,14 +21,21 @@ content.style.opacity='1';
 music.play();
 });
 
+/* Scroll Reveal */
 const memories=document.querySelectorAll('.memory');
 
 window.addEventListener('scroll',()=>{
 memories.forEach(mem=>{
 const position=mem.getBoundingClientRect().top;
-const screen=window.innerHeight;
-if(position<screen-100){
+if(position<window.innerHeight-100){
 mem.classList.add('show');
 }
 });
+});
+
+/* Cursor Glow */
+const cursor=document.getElementById("cursorGlow");
+document.addEventListener("mousemove",(e)=>{
+cursor.style.left=e.clientX+"px";
+cursor.style.top=e.clientY+"px";
 });
