8ab3c833fd3ba90fb68ea3f094161d28367d9c8c
brandportfolio.studio <ameyg1806@gmail.com>
2026-02-13 00:40:53 +0530

    Refactor scroll animation and implement typewriter effect
    Updated scroll animation comment and added typewriter effect functionality.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ yesBtn.addEventListener("click",()=>{
   music.play();
 });
 
-/* Scroll Animation */
+/* Scroll Reveal */
 
 const memories = document.querySelectorAll(".memory");
 
@@ -27,4 +27,35 @@ window.addEventListener("scroll",()=>{
       mem.classList.add("show");
     }
   });
+});
+
+/* Typewriter Letter */
+
+const text = `It was just a random app .. befriend.. it was 1st of December 2025 ..
+one random right swipe a random profile it felt like something but i didn't know what it was..
+but then she accepted my request and we started talking and then each day i started feeling something..
+each day sharing snaps chats and random voice notes ..
+and also we became comfortable so quick like hume pata bhi nhi chala how it happened
+then on Friday, 5th of dec i just thought of telling her i feel something..
+but i jst avoided it and thought for 2 days that I'm sure or not ..
+or it's jst some sort of attraction..
+but it was not jst attraction..
+it was something more than love more than what words can't explain..
+but i jst gathered the guts of confessing my feelings to her..
+and i thought now I'm gone.. khel khatam ..
+but idk how but she felt the same
+we shared our insta and from there we can say december theory is real..
+jst a random hii turned into a lifetime bond..`;
+
+let i = 0;
+function typeWriter(){
+  if(i < text.length){
+    document.getElementById("typeText").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 25);
+  }
+}
+
+yesBtn.addEventListener("click",()=>{
+  setTimeout(typeWriter,1000);
 });
